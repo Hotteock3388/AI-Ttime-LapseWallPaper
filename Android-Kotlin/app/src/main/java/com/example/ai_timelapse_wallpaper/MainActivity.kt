0766177d2a598daf120446185357145eda8cc258
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2020-11-08 22:33:41 +0900

    [ Add ] MainActivity.xml 추가

--- Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/MainActivity.kt
+++ Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.ai_timelapse_wallpaper
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.app.WallpaperManager
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var wallpaperManager : WallpaperManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        wallpaperManager = WallpaperManager.getInstance(applicationContext)
+
+        clearWallPaper.setOnClickListener {
+            wallpaperManager.clearWallpaper()
+
+        }
+        selectWallPaper.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    private fun openGallery() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 100){
+            val imageUri : Uri? = data?.data
+
+            //Uri 제대로 왔는지 테스트
+            //imageView.setImageURI(imageUri)
+
+            val image : Bitmap = MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, imageUri)
+
+            try {
+                wallpaperManager.setBitmap(image)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+
+        }
     }
 }
