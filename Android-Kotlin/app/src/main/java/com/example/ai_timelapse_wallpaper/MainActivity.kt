6a7f37ebd92b44922d9d90a7f623e94870eca7ce
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2020-11-08 22:38:04 +0900

    [ Add ] 커밋 잘못했지만 배경화면 초기화, 배경화면 설정 구현

--- Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/MainActivity.kt
+++ Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/MainActivity.kt
@@ -30,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openGallery() {
+        // 암시적 인텐트로 갤러리 열기
         val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         startActivityForResult(gallery, 100)
     }
@@ -39,9 +40,9 @@ class MainActivity : AppCompatActivity() {
         if(resultCode == Activity.RESULT_OK && requestCode == 100){
             val imageUri : Uri? = data?.data
 
-            //Uri 제대로 왔는지 테스트
+            //Uri 제대로 왔는지 테스트하는 코드
             //imageView.setImageURI(imageUri)
-
+            
             val image : Bitmap = MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, imageUri)
 
             try {
