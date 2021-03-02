a2073253ee76b4300f2d21fad4b310489ace4fe9
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-03-02 18:28:26 +0900

    MainViewModel 생성

---
+++ Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/activity/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ai_timelapse_wallpaper.activity
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    //val imageArrs = ArrayList<Uri>()
+}
