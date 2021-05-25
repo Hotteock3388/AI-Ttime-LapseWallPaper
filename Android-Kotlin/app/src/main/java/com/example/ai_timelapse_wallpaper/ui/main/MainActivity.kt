47c659b00b88fdc09e899ab8f4316cf2f2beb13f
Hotteock3388 <57486593+Hotteock3388@users.noreply.github.com>
2021-05-25 22:35:54 +0900

    미리 테스트 해봤던 기능 코드 제거, MainActivity의 기능 추가

--- Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/ui/main/MainActivity.kt
+++ Android-Kotlin/app/src/main/java/com/example/ai_timelapse_wallpaper/ui/main/MainActivity.kt
@@ -1,99 +1,44 @@
 package com.example.ai_timelapse_wallpaper.ui.main
 
-import android.app.Activity
-import android.app.ActivityManager
-import android.app.WallpaperManager
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
-import android.view.WindowManager
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.ai_timelapse_wallpaper.R
-import com.example.ai_timelapse_wallpaper.data.local.SharedPref
-import com.example.ai_timelapse_wallpaper.service.ChangeWallPaperService
+import com.example.ai_timelapse_wallpaper.databinding.ActivityMainBinding
+import com.example.ai_timelapse_wallpaper.ui.setting.SettingActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    val PICTURE_REQUEST_CODE = 2141
+    private lateinit var viewModel: MainViewModel
 
-    private lateinit var wallpaperManager : WallpaperManager
+    private lateinit var binding: ActivityMainBinding
 
-    private lateinit var mainViewModel: MainViewModel
 //    private lateinit var binding: AcBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        wallpaperManager = WallpaperManager.getInstance(applicationContext)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
-        removeTitleAction()
-        if(!this.isServiceRunning(ChangeWallPaperService::class.java)){
-            startForegroundService(Intent(applicationContext, ChangeWallPaperService::class.java))
+        with(binding){
+            lifecycleOwner = this@MainActivity
+            activity = this@MainActivity
+            vm = viewModel
         }
+        val adapter = MAdapter(viewModel.getDummyList(baseContext))
+        viewPager_MainActivity.adapter = adapter
+        adapter.notifyDataSetChanged()
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD)
     }
 
-    fun removeTitleAction(){
-        // hide actionBar
-        val actionBar = supportActionBar
-        actionBar?.hide()
-
-        //hide titleBar(fullScreen)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    fun imageSetting(){
+        startActivity(Intent(this, SettingActivity::class.java))
     }
-
-
-    //서비스가 이미 실행중인지 확인
-    fun Context.isServiceRunning(serviceClass: Class<*>): Boolean {
-        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-
-        for (service in activityManager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.name == service.service.className) {
-                Log.d("isServiceRunning", "Service is running")
-                return true
-            }
-        }
-        return false
-    }
-
-    private fun openGallery() {
-        // 암시적 인텐트로 갤러리 열기
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = MediaStore.Images.Media.CONTENT_TYPE
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-
-        //Intent.createChooser 어디에서 사진을 고를건지 선택하게 해줌(갤러리, 포토, 클라우드)
-        startActivityForResult(Intent.createChooser(intent, "배경화면 선택"), PICTURE_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == PICTURE_REQUEST_CODE){
-            val imageUri : Uri? = data?.data
-
-            val clipData = data?.clipData
-            var i = 0
-
-            var arr = ArrayList<Uri>()
-            for (i in 0 until 6){
-                arr.add(clipData!!.getItemAt(i).uri)
-            }
-            SharedPref(applicationContext).saveImageArr(arr)
-
-            //val image : Bitmap = MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, imageUri)
-
-        }
-    }
-
 }
